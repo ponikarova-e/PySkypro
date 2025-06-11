219cc29b43964b8c7cd7e01696862d21109e6f36
ponikarova-e <ponikarova-e@mail.ru>
2025-06-11 22:11:53 +0500

    Lesson_1

---
+++ lesson1/lession_1_task_5.py
@@ -0,0 +1,14 @@
+def Numbers(num):
+    print(num,end="")
+
+Numbers(8)
+Numbers(8)
+Numbers(0)
+Numbers(0)
+Numbers(5)
+Numbers(5)
+Numbers(5)
+Numbers(3)
+Numbers(5)
+Numbers(3)
+Numbers(5)
---
+++ lesson1/lesson_1_task_2.py
@@ -0,0 +1,2 @@
+my_age=50
+print(my_age)
